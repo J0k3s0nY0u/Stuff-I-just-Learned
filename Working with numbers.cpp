32f5592b3ff814af610af1d4d107de4f6c1a319b
J0k3s0nY0u <109585635+J0k3s0nY0u@users.noreply.github.com>
2022-11-14 21:33:14 +0000

    Add files via upload

---
+++ Working with numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	
+	cout << (4.0 + 3) / 19 << endl;
+	cout << ceil(50.3849) << endl;
+	cout << floor(23.474) << endl;
+	cout << fmin(5, 7) << endl;
+	
+	
+	return 0;
+}
