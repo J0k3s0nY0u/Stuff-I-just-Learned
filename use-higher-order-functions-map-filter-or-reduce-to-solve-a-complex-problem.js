5d6fb49c8949b6b384abe8d445dbc9c3f7bafe37
J0k3s0nY0u <109585635+J0k3s0nY0u@users.noreply.github.com>
2022-12-16 21:00:19 +0100

    Update and rename use-higher-order-functions-map-filter-or-reduce-to-solve-a-complex-problem.txt to use-higher-order-functions-map-filter-or-reduce-to-solve-a-complex-problem.js

--- use-higher-order-functions-map-filter-or-reduce-to-solve-a-complex-problem.js
+++ use-higher-order-functions-map-filter-or-reduce-to-solve-a-complex-problem.js
@@ -1,9 +1,7 @@
 const squareList = arr => {
-  // Only change code below this line
   return arr
   .filter(item => item > 0 && item % parseInt(item) === 0)
   .map(item => Math.pow(item, 2));
-  // Only change code above this line
 };
 
 const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
